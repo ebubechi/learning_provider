472a0ff0a441fe47398981e7a4732d46928bc87a
ebubechi <emmanuelsonzico@gmail.com>
2023-07-31 18:50:10 +0100

    third example

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,31 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => ObjectProvider(),
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.amber),
-      home: const HomePage(),
-    ),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.amber),
+    home: const HomePage(),
   ));
 }
 
+String now() => DateTime.now().toIso8601String();
+
 @immutable
-class BaseOject {
-  final String id;
-  final String lastUpdated;
-  BaseOject()
-      : id = const Uuid().v4(),
-        lastUpdated = DateTime.now().toIso8601String();
-
-  @override
-  bool operator ==(covariant BaseOject other) => id == other.id;
-
-  @override
-  int get hashCode => id.hashCode;
+class Seconds {
+  final String value;
+  Seconds() : value = now();
 }
 
 @immutable
-class ExpensiveObject extends BaseOject {}
-
-@immutable
-class CheapObject extends BaseOject {}
-
-class ObjectProvider extends ChangeNotifier {
-  late String id;
-  late CheapObject _cheapObject;
-  late StreamSubscription _cheapObjectStreamSubs;
-  late ExpensiveObject _expensiveObject;
-  late StreamSubscription _expensiveOjectStreamSubs;
-
-  CheapObject get cheapObject => _cheapObject;
-  ExpensiveObject get expensiveObject => _expensiveObject;
-
-  ObjectProvider()
-      : id = const Uuid().v4(),
-        _cheapObject = CheapObject(),
-        _expensiveObject = ExpensiveObject() {
-    start();
-  }
-
-  @override
-  void notifyListeners() {
-    id = const Uuid().v4();
-    super.notifyListeners();
-  }
-
-  void start() {
-    _cheapObjectStreamSubs =
-        Stream.periodic(const Duration(seconds: 1)).listen((_) {
-      _cheapObject = CheapObject();
-      notifyListeners();
-    });
-    _expensiveOjectStreamSubs =
-        Stream.periodic(const Duration(seconds: 10)).listen((_) {
-      _expensiveObject = ExpensiveObject();
-      notifyListeners();
-    });
-  }
-
-  void stop() {
-    _cheapObjectStreamSubs.cancel();
-    _expensiveOjectStreamSubs.cancel();
-  }
+class Minutes {
+  final String value;
+  Minutes() : value = now();
 }
+
+// Stream<String> newStream(Duration duration) =>
+//     Stream.periodic(duration, (_) => now());
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -86,98 +35,62 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Hey There!'),
       ),
-      body: Column(
-        children: [
-          const Row(
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+              const Duration(seconds: 5),
+              (_) => Minutes(),
+            ),
+            initialData: Minutes(),
+          ),
+        ],
+        child: const Column(children: [
+          Row(
             children: [
-              Expanded(child: CheapWidget()),
-              Expanded(child: ExpensiveWidget()),
+              SecondsWidget(),
+              MinutesWidget(),
             ],
           ),
-          SizedBox(
-              width: MediaQuery.sizeOf(context).width,
-              child: const ObjectProviderWidget()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                  onPressed: () {
-                    context.read<ObjectProvider>().stop();
-                  },
-                  child: const Text('Stop')),
-              TextButton(
-                  onPressed: () {
-                    context.read<ObjectProvider>().start();
-                  },
-                  child: const Text('Start')),
-            ],
-          )
-        ],
+        ],)
       ),
     );
   }
 }
 
-class ExpensiveWidget extends StatelessWidget {
-  const ExpensiveWidget({super.key});
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final expensiveObject = context.select<ObjectProvider, ExpensiveObject>(
-      (provider) => provider.expensiveObject,
-    );
-    return Container(
-      height: 100,
-      color: Colors.blue,
-      child: Column(
-        children: [
-          const Text('Expensive Widget'),
-          const Text('Last updated'),
-          Text(expensiveObject.lastUpdated),
-        ],
-      ),
-    );
-  }
-}
-
-class CheapWidget extends StatelessWidget {
-  const CheapWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final cheapObject = context.select<ObjectProvider, CheapObject>(
-      (provider) => provider.cheapObject,
-    );
-    return Container(
-      height: 100,
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+        child: Container(
       color: Colors.yellow,
-      child: Column(
-        children: [
-          const Text('Cheap Widget'),
-          const Text('Last updated'),
-          Text(cheapObject.lastUpdated),
-        ],
-      ),
-    );
+      height: 100,
+      child: Text(seconds.value),
+    ));
   }
 }
 
-class ObjectProviderWidget extends StatelessWidget {
-  const ObjectProviderWidget({super.key});
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = context.watch<ObjectProvider>();
-    return Container(
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+        child: Container(
+      color: Colors.blue,
       height: 100,
-      color: Colors.purple,
-      child: Column(
-        children: [
-          const Text('Object Provider Widget'),
-          const Text('ID'),
-          Text(provider.id),
-        ],
-      ),
-    );
+      child: Text(minutes.value),
+    ));
   }
 }
